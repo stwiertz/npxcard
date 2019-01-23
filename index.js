23d97a8403e2441fbe0611b3e8202557054c0fdc
stwiertz <stephanewiertz@gmail.com>
2019-01-23 16:13:55 +0100

    fix typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const boxen = require('boxen');
 const chalk = require('chalk'); 
 
 
-const text= '    Stephane Wiertz' +chalk.yellow('/') + "stwiertz"  + "\n\n" + "    Work :  Student Becodek" + "\n\n" + chalk.yellow("   --------------------------") + "\n\n"  + "Github:  https://github.com/" + chalk.blue("stwiertz") + "\n"  + "npm:  https://npmls.com/" + chalk.green("stwiertz") + "\n" + "linkedin: https://www.linkedin.com/in/" + chalk.red("stephane-wiertz")
+const text= '    Stephane Wiertz' +chalk.yellow('/') + "stwiertz"  + "\n\n" + "    Work :  Student Becode" + "\n\n" + chalk.yellow("   --------------------------") + "\n\n"  + "Github:  https://github.com/" + chalk.blue("stwiertz") + "\n"  + "npm:  https://npmls.com/" + chalk.green("stwiertz") + "\n" + "linkedin: https://www.linkedin.com/in/" + chalk.red("stephane-wiertz")
 
 
 console.log(boxen(text, {padding: 1 ,borderStyle:"classic"}));
